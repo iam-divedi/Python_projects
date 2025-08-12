35f9e8ab12c3dcdd85c60e885207aa1c5fda4c0a
iam-divedi <69568648+iam-divedi@users.noreply.github.com>
2025-08-12 21:51:41 +0530

    Update Buzz_program.py

--- Buzz_program.py
+++ Buzz_program.py
@@ -1,18 +1,18 @@
-print('FIZZ BUZZ PROGRAM')
+print('BUZZ FIZZ PROGRAM')
+
 till_num = int(input('Enter the specified number: '))
 my_list = []
 
 for num in range(1, till_num + 1):
-    result = ""
-
-    if num % 3 == 0:
-        result += 'Fizz'
-    if num % 5 == 0:
-        result += 'Buzz'
-
-    if result == "":
+    if num % 5 == 0 and num % 3 == 0:
+        result = 'BuzzFizz'
+    elif num % 5 == 0:
+        result = 'Buzz'
+    elif num % 3 == 0:
+        result = 'Fizz'
+    else:
         result = num
-
+    
     my_list.append(result)
 
 print(my_list)
