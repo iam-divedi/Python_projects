0d59be53fcc310ea18f503a033992fe4b6c1732a
iam-divedi <69568648+iam-divedi@users.noreply.github.com>
2025-10-09 23:09:19 +0530

    Update Buzz_program.py

--- Buzz_program.py
+++ Buzz_program.py
@@ -1,6 +1,6 @@
 print('BUZZ FIZZ PROGRAM')
 
-till_num = int(input('Enter the specified number: '))
+till_num = int(input('Enter the specified number: '))//input
 my_list = []
 
 for num in range(1, till_num + 1):
